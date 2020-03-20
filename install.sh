50841b51fee9975f9db77d754dc711f7678315ea
john-theo <xinhaozhuang.work@gmail.com>
2020-03-20 16:45:47 +0800

    .

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+set -e
+
 echo "-> Step 1/3"
 echo "Creating virtual environment in current folder..."
-virtualenv venv
-./venv/bin/activate
+if [ -d venv ]; then
+  echo "Virtual environment already existed skipping..."
+else
+  pip3 install virtualenv
+  virtualenv venv
+  source venv/bin/activate
+fi
 
 echo
 echo "-> Step 2/3"
@@ -13,6 +21,18 @@ function start-docker() {
     echo "Warning: Data in this database will be erased once the container is removed!"
     echo "   This project if for demo only! **DO NOT** use this in production environment."
     docker-compose up -d
+    echo "Waiting for neo4j to start..."
+    secs=10
+    prefix="Browser will start in"
+    suffix="seconds..."
+    while [ $secs -gt 0 ]; do
+       echo -ne "$prefix $secs $suffix\r"
+       sleep 1
+       : $((secs--))
+    done
+    echo "Bringing up the browser..."
+    xdg-open "http://localhost:7474" &
+    sleep 1
 }
 
 function start-local-neo4j() {
@@ -22,9 +42,7 @@ function start-local-neo4j() {
 echo
 echo "-> Step 3/3"
 echo "Use neo4j docker instead of local neo4j server instance?"
-echo "- Yes(recommended) / No"
-echo "Please input: "
-select yn in "Yes" "No" "yes" "no", "y", "n", "Y", "N"; do
+select yn in "Yes" "No"; do
     case $yn in
         Yes|yes|Y|y ) start-docker; break;;
         No|no|N|n   ) break;;
