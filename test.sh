4263881688580c5b45dcdd528025dbf93284a3ee
john-theo <xinhaozhuang.work@gmail.com>
2020-03-21 03:17:04 +0800

    .

--- test.sh
+++ test.sh
@@ -6,8 +6,10 @@ set -e
 read -p "Press ENTER when neo4j instance in started..."
 echo
 
+source venv/bin/activate
 python -m src
 
+echo
 echo "Run './clean.sh' to remove Neo4j configs and DB completely!"
 #./clean.sh
 echo "Bye!"
